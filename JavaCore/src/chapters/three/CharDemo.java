0dec9d74276559d2161e7a371e9ed32d7957cd87
Evewibe <aleksey.dantist@gmail.com>
2024-07-27 07:59:24 +0600

    feat: chardemo2.java and fix chardemo.java

--- JavaCore/src/chapters/three/CharDemo.java
+++ JavaCore/src/chapters/three/CharDemo.java
@@ -1,10 +1,11 @@
 package chapters.three;
 
+// Продемонстрировать применение типа данных Char
 public class CharDemo {
     public static void main(String args[]){
         char ch1, ch2;
 
-        ch1 = 88;
+        ch1 = 88; // код символа Х
         ch2 = 'Y';
 
         System.out.print("ch1 and ch2: ");
