3274e0811a2cf184f1c1b334ee6dd288991a41df
Evewibe <aleksey.dantist@gmail.com>
2024-07-27 07:01:05 +0600

    feat: example2.java

---
+++ JavaCore/src/chapters/two/Example2.java
@@ -0,0 +1,21 @@
+package chapters.two;
+
+/*
+    Это еще один пример короткой программы.
+    Присвоить исходному файлу имя "Example2.java"
+ */
+public class Example2 {
+    public static void main(String args[]){
+        int num; // в этой строке кода объявляется
+                // переменная с именем num
+        num = 100; // в этой строке кода переменной num
+                   // присваивается значение 100
+        
+        System.out.println("Это переменная num: " + num);
+
+        num = num * 2;
+
+        System.out.println("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
