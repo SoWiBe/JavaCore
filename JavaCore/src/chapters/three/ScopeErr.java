2904b466de494f27c6dc2e577195f4c638338fc0
Evewibe <aleksey.dantist@gmail.com>
2024-07-27 09:33:30 +0600

    feat: scopeerr.java

---
+++ JavaCore/src/chapters/three/ScopeErr.java
@@ -0,0 +1,12 @@
+package chapters.three;
+
+//Скомпилировать эту программу нельзя
+public class ScopeErr {
+    public static void main(String args[]){
+        int bar = 1;
+        {                // создается новая область действия
+            int bar = 2; // Ошибка во время компиляции -
+                         // переменная bar уже определена
+        }
+    }
+}
