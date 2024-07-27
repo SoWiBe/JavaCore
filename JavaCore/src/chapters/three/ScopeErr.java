340c0bf2b445d2215c2efb3c510d9d33319a732f
Evewibe <aleksey.dantist@gmail.com>
2024-07-27 09:38:16 +0600

    feat: conversion.java

--- JavaCore/src/chapters/three/ScopeErr.java
+++ JavaCore/src/chapters/three/ScopeErr.java
@@ -5,7 +5,7 @@ public class ScopeErr {
     public static void main(String args[]){
         int bar = 1;
         {                // создается новая область действия
-            int bar = 2; // Ошибка во время компиляции -
+            //int bar = 2; // Ошибка во время компиляции -
                          // переменная bar уже определена
         }
     }
