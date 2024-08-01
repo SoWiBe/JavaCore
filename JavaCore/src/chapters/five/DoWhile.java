535d0eefd79a35f13826fbd0d6bc033802778ce3
Evewibe <aleksey.dantist@gmail.com>
2024-08-01 13:46:01 +0600

    feat: DoWhile.java

---
+++ JavaCore/src/chapters/five/DoWhile.java
@@ -0,0 +1,13 @@
+package chapters.five;
+
+//Демонстрация рабоыт цикла do-while
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+
+        do {
+            System.out.println("Импульс номер " + n);
+            n--;
+        } while (n > 0);
+    }
+}
