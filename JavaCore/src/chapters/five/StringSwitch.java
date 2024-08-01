70aa840af8fa6154ffba08987aca0c2009ff5742
Evewibe <aleksey.dantist@gmail.com>
2024-08-01 13:41:11 +0600

    feat: StringSwitch.java

---
+++ JavaCore/src/chapters/five/StringSwitch.java
@@ -0,0 +1,22 @@
+package chapters.five;
+
+//Применение строки для управления оператором switch.
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "two";
+        switch(str){
+            case "one":
+                System.out.println("один");
+                break;
+            case "two":
+                System.out.println("два");
+                break;
+            case "three":
+                System.out.println("три");
+                break;
+            default:
+                System.out.println("совпадений нет");
+                break;
+        }
+    }
+}
