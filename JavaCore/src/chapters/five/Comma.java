6762c4e8c8f826dfeb9241a2449031590ab699ba
Evewibe <aleksey.dantist@gmail.com>
2024-08-02 21:28:21 +0600

    feat: comma.java

--- JavaCore/src/chapters/five/Comma.java
+++ JavaCore/src/chapters/five/Comma.java
@@ -1,7 +1,12 @@
 package chapters.five;
 
+//Использование запятой
 public class Comma {
     public static void main(String[] args) {
-        
+        int a, b;
+        for (a = 1, b = 2; a < b; a++, b--) {
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
     }
 }
