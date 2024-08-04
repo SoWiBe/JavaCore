ca96c2e463ca9b7ccbe1be13079745d7c5d0f215
Evewibe <aleksey.dantist@gmail.com>
2024-08-04 23:48:24 +0600

    feat: nochange.java

---
+++ JavaCore/src/chapters/five/NoChange.java
@@ -0,0 +1,19 @@
+package chapters.five;
+
+public class NoChange {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,45,6,7,8,9,10};
+        
+        for (int x: nums){
+            System.out.println(x + " ");
+            x = x * 10;
+        }
+
+        System.out.println();
+        for (int x: nums){
+            System.out.println(x + " ");
+        }
+
+        System.out.println();
+    }
+}
